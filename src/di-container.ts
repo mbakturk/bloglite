ea15cc9dd77a6961fe55653f4940c5ad1a2d9beb
mb.akturk <mb.akturk@gmail.com>
2019-02-06 22:21:57 +0300

    feat: markdown and twemoji dependencies are removed. webpack is added to project

--- src/di-container.ts
+++ src/di-container.ts
@@ -1,5 +1,4 @@
 import { Container } from "inversify";
-import { MarkdownIt } from "markdown-it";
 import { Config, config } from './config-reader';
 import { DashboardController } from './controller/dashboard.controller';
 import { EditorController } from './controller/editor.controller';
@@ -10,10 +9,6 @@ import { Database } from "./database";
 import { LoggerCreator, SysLogger } from './logger';
 import { PostDAO } from './repo/post.dao';
 import { UserDAO } from './repo/user.dao';
-import markdownit = require("markdown-it");
-import hljs = require("highlight.js");
-import markdownItEmoji = require("markdown-it-emoji")
-import twemoji = require("twemoji");
 
 const container = new Container();
 container.bind<Config>("Config").toConstantValue(config);
@@ -28,38 +23,6 @@ container.bind<DashboardController>(DashboardController).toSelf();
 container.bind<EditorController>(EditorController).toSelf();
 container.bind<PostController>(PostController).toSelf();
 container.bind<RSSController>(RSSController).toSelf();
-
-
-// Initialize markdown-it
-const md: MarkdownIt = new markdownit({
-    highlight: function (str, lang) {
-        if (lang && hljs.getLanguage(lang)) {
-            try {
-                return '<pre class="hljs"><code>' +
-                    hljs.highlight(lang, str, true).value +
-                    '</code></pre>';
-            } catch (__) { }
-        }
-
-        return '<pre class="hljs"><code>' + md.utils.escapeHtml(str) + '</code></pre>';
-    },
-    html: true,
-    linkify: true,
-    typographer: true
-}).use(markdownItEmoji).use(
-    require('markdown-it-video'), { // <-- this use(package_name) is required
-        youtube: { width: 640, height: 390 },
-        vimeo: { width: 500, height: 281 },
-        vine: { width: 600, height: 600, embed: 'simple' },
-        prezi: { width: 550, height: 400 }
-    });
-
-md.renderer.rules.emoji = function (token, idx) {
-    return twemoji.parse(token[idx].content);
-};
-
-container.bind<MarkdownIt>('MarkdownIt').toConstantValue(md);
-
 
 export { container };
 
